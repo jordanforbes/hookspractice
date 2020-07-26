8b4b7539982d7b169e1acaf8c5aa93effa000f68
jordanforbes <jordforb@gmail.com>
2020-07-26 18:41:53 -0400

    finished project

--- src/App.js
+++ src/App.js
@@ -5,18 +5,29 @@ import './App.css';
 
 const App =()=>{
     const [ username, setUsername ] = useState("");
-    return(
-        <div>
-            <input 
-                type = "text"
-                placeholder = "Enter Username"
-                value={username}
-                onChange={e => {
-                    setUsername(e.target.value);
-                }}
-            />
-        </div>
-    )
+    const [ password, setPassword ] = useState("");
+    const [ showPassword, setShowPassword ] = useState(true)
+
+    return (
+      <div className="container">
+        <input
+          type="text"
+          placeholder="Enter Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+
+        <input
+          type={showPassword ? 'text' : 'password'}
+          placeholder="Enter Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <h2>{username}</h2>
+        <h2>{showPassword ? password : '*'.repeat(password.length)}</h2>
+        <button onClick={e=> setShowPassword(!showPassword)} >Show/Hide Password</button>
+      </div>
+    );
 }
 
 export default App
