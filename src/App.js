a90e411e3a28d2cc5ca87182ed10c9d459685259
jordanforbes <jordforb@gmail.com>
2020-07-26 17:08:19 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+
+
+import './App.css';
+
+const App =()=>{
+    const [ username, setUsername ] = useState("");
+    return(
+        <div>
+            <input 
+                type = "text"
+                placeholder = "Enter Username"
+                value={username}
+                onChange={e => {
+                    setUsername(e.target.value);
+                }}
+            />
+        </div>
+    )
+}
+
+export default App
